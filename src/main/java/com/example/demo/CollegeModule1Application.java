bd9dae9e9453cc5f8963257497bb92029e393f1d
Muskan <katariamuskan02@gmail.com>
2024-03-15 10:11:42 +0530

    First Commit

---
+++ src/main/java/com/example/demo/CollegeModule1Application.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class CollegeModule1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollegeModule1Application.class, args);
+	}
+
+}
